6509763c734c52a1d25cbbcde6dfd5dfaa93b712
Tia <tia@cykod.com>
2010-03-25 12:35:25 -0400

    dependent is not spelled an A.  oof

--- app/models/book_page.rb
+++ app/models/book_page.rb
@@ -11,7 +11,7 @@ class BookPage < DomainModel
 
   validates_presence_of :book_book
 
-  has_many :book_page_versions, :dependant => :destroy
+  has_many :book_page_versions, :dependent => :destroy
 
   apply_content_filter(:body => :body_html)  do |page|
     { :filter => page.book_book.content_filter,
